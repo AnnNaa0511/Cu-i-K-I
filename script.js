24017e774b90a5efaacdd1c6db22f466cea8c439
AnnNaa0511 <amhienpkk@gmail.com>
2024-05-13 16:19:33 +0700

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+var element = document.getElementsByClassName("add");
+
+function myFunction() {
+    alert("hihihi");
+}
+
+// element.addEventListener("click" , function() {
+//     alert("Thêm mới!!");
+// });
